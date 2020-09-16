7e22b06bc5cbe924e730e2f8b9026561a0715d59
Douglas Pinheiro <pinheiro.ep@hotmail.com>
2020-09-16 16:21:53 -0300

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ Course/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            bool completo = false;
+            char genero = 'f';
+            char letra = '\u0041';
+            byte n1 = 255;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L;
+            float n5 = 4.5f;
+            double n6 = 4.556226632;
+            string nome = "Maria Green";
+            object obj1 = "Alex Brown";
+            object obj2 = 4.5f;
+            int n7 = int.MinValue;
+            int n8 = int.MaxValue;
+
+            Console.WriteLine(n8);
+            Console.WriteLine(n7);
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(letra);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+            Console.WriteLine(n6.ToString("F2"));
+        }
+    }
+}
